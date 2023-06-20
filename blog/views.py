811361b418a7136b6368ac08627125712911820b
shreyanzjain <shreyanz32@gmail.com>
2023-06-20 13:18:47 +0530

    "change timezone, add post"

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'title': 'Post 1',
-        'author': 'Shreyans',
-        'date_posted': 'Aug 27, 2021',
-        'content': 'This is post 1'
-    },
-    {
-        'title': 'Post 2',
-        'author': 'Pinky',
-        'date_posted': 'Aug 28, 2021',
-        'content': 'This is post 2'
-    }
-]
+# posts = [
+#     {
+#         'title': 'Post 1',
+#         'author': 'Shreyans',
+#         'date_posted': 'Aug 27, 2021',
+#         'content': 'This is post 1'
+#     },
+#     {
+#         'title': 'Post 2',
+#         'author': 'Pinky',
+#         'date_posted': 'Aug 28, 2021',
+#         'content': 'This is post 2'
+#     }
+# ]
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
