99f3d616c09021fa170c6b870288f35bdfb4b1ee
shreyanzjain <shreyanz32@gmail.com>
2023-06-20 12:16:38 +0530

    set urls, and views. Created Home and base templates.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'title': 'Post 1',
+        'author': 'Shreyans',
+        'date_posted': 'Aug 27, 2021',
+        'content': 'This is post 1'
+    },
+    {
+        'title': 'Post 2',
+        'author': 'Pinky',
+        'date_posted': 'Aug 28, 2021',
+        'content': 'This is post 2'
+    }
+]
 # Create your views here.
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/home.html')
