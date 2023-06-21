543beda25fc49f94bf3331ea9318d76596329891
shreyanzjain <shreyanz32@gmail.com>
2023-06-21 14:47:50 +0530

    milestone: implemented a user-posts view

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from blog import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', login_required(views.PostListView.as_view()), name='blog-home'),
+    path('post/<int:pk>/', login_required(views.PostDetailView.as_view()), name='blog-post-detail'),
+    path('<pk>/posts/', login_required(views.UserPostListView.as_view()), name='blog-user-posts'),
     path('about', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView
 from .models import Post
+from django.contrib.auth.models import User
 
 # posts = [
 #     {
@@ -17,12 +19,20 @@ from .models import Post
 #     }
 # ]
 # Create your views here.
-@login_required()
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name='blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+class UserPostListView(DetailView):
+    model = User
+    template_name='blog/user_post.html'
 
 @login_required()
 def about(request):
